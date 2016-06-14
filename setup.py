0670260b44a9c49d23ff5170e09db606eba52fac
Arthur Brenaut <abrenaut@rmn.com>
2016-06-14 15:17:10 +0200

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 5:
     sys.exit('Sorry, Python < 3.5 is not supported')
 
 setup(name='waybackscraper',
-      version='0.1',
+      version='0.2',
       description='Scrapes a website archives on the wayback machine using asyncio.',
       author='Arthur Brenaut',
       author_email='arthur.brenaut@gmail.com',
