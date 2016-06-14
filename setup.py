d3caf3a0aeaa6ad89f66ff1b0f95e30380e57cb7
Arthur Brenaut <abrenaut@rmn.com>
2016-06-14 15:20:30 +0200

    New console script entry point

--- setup.py
+++ setup.py
@@ -5,11 +5,14 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 5:
     sys.exit('Sorry, Python < 3.5 is not supported')
 
 setup(name='waybackscraper',
-      version='0.2',
+      version='0.3',
       description='Scrapes a website archives on the wayback machine using asyncio.',
       author='Arthur Brenaut',
       author_email='arthur.brenaut@gmail.com',
       packages=find_packages(),
+      entry_points={
+          'console_scripts': ['waybackscraper=waybackscraper.cli:main'],
+      },
       install_requires=[
           'aiohttp',
           'lxml'
